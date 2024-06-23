c265b2ce7469a2bf9db26ba9dfaf11367cc7f5ad
miras_ai <107981665+MirasBaisbay@users.noreply.github.com>
2024-06-23 18:25:52 +0500

    Add files via upload

---
+++ RNNs.py
@@ -0,0 +1,108 @@
+# Imports
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+# Set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Hyperparameters
+input_size = 28
+sequence_length = 28
+num_layers = 2
+hidden_size = 256
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 5
+
+
+# Create a simple RNN
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size * sequence_length, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+        out, _ = self.rnn(x, h0)
+        out = out.reshape(out.shape[0], -1)
+        out = self.fc(out)
+        return out
+
+
+# Load the data
+train_dataset = datasets.MNIST(root='datasets/',
+                               train=True,
+                               transform=transforms.ToTensor(),
+                               download=True)
+test_dataset = datasets.MNIST(root='datasets/',
+                              train=False,
+                              transform=transforms.ToTensor(),
+                              download=True)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+# Initialize the network
+model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the network
+
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # Get the data and targets
+        data = data.to(device).squeeze(1)
+        targets = targets.to(device)
+
+        # forward
+        predictions = model(data)
+        loss = criterion(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+# Check accuracy on training & testing
+def check_accuracy(loader, model):
+    num_correct = 0
+    num_samples = 0
+
+    # Set model to eval
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device).squeeze(1)
+            y = y.to(device=device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+    model.train()
+    return num_correct / num_samples
+
+
+print(f"Accuracy on training set: {check_accuracy(train_loader, model)*100:2f}")
+print(f"Accuracy on test set: {check_accuracy(test_loader, model)*100:.2f}")
+
+
+# Accuracy on training set: 96.866669
+# Accuracy on test set: 96.51
